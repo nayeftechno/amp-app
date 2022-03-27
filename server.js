03b0244b521969b3acd16c1770f56273b391dca3
nayeftechno <nayeftechno@gmail.com>
2022-03-27 16:15:45 +0300

    feat: deploy to heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set('view engine','ejs');
 
 app.get('/mweb',(request,response)=>{
     response.render('mweb/index');
 });
 
-app.listen(3000);
+app.listen(port);
